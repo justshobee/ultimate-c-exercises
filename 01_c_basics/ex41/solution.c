380229c18bd028a071b0d5fd7c43facce4dca369
shobeedev <moha.saoud22@gmail.com>
2026-03-09 11:07:56 +0000

    exercise ex41

--- 01_c_basics/ex41/solution.c
+++ 01_c_basics/ex41/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/09 10:14:11 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/09 11:06:25 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/09 11:07:44 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ int main()
 	printf("Enter Car Model Year: ");
 	scanf("%d",&(*s2).model_year);
 	printf("Enter Car Color: ");
-	scanf("%s",&(*s2).color);
+	scanf("%s",&s2->color);
 
 	printf("\n--- Car Details (via Pointer) ---\n");
-	printf("Model year : %d\n",(*s2).model_year);
+	printf("Model year : %d\n",s2->model_year);
 	printf("Color : %s\n",(*s2).color);
 
 	return 0;
