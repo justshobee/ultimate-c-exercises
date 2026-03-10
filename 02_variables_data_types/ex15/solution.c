ad75bb6519cf5f8ff97f5ac2a64afcc85c4c7630
shobeedev <moha.saoud22@gmail.com>
2026-03-10 21:36:52 +0000

    exercise ex15

---
+++ 02_variables_data_types/ex15/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 21:33:12 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 21:36:39 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	const float PI = 3.1459;
+
+	// assignment of read-only variable PI
+	//PI = 4;
+	printf("PI = %f\n",PI);
+
+	return 0;
+}
