edfc00407bf76df8bc1ae7ab49ec68175fd63ccb
shobeedev <moha.saoud22@gmail.com>
2026-03-10 12:06:51 +0000

    exercise ex11

---
+++ 02_variables_data_types/ex11/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 12:02:56 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 12:06:35 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int num = 2147483647;
+
+	printf("Maximum value of int: %d\n",num);
+	num++;
+	printf("After incrementing: %d\n",num);
+
+	return 0;
+}
