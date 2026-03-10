ff09e47d5b1a84932ad02a237a4cde3210fc8efb
shobeedev <moha.saoud22@gmail.com>
2026-03-10 12:14:42 +0000

    exercise ex11

--- 02_variables_data_types/ex11/solution.c
+++ 02_variables_data_types/ex11/solution.c
@@ -6,19 +6,20 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/10 12:02:56 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/10 12:06:35 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/10 12:14:31 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-
+#include <limits.h>
 int main()
 {
-	int num = 2147483647;
+	//to get int max from terminal use : getconf INT_MAX
+	int max_int = INT_MAX;
 
-	printf("Maximum value of int: %d\n",num);
-	num++;
-	printf("After incrementing: %d\n",num);
+	printf("Maximum value of int: %d\n",max_int);
+	int overflow_int = max_int + 1;
+	printf("After incrementing: %d\n",overflow_int);
 
 	return 0;
 }
