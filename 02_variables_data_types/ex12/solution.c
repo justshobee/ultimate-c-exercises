622eeb7afe5c86cef40bdd58e70db40df5d76533
shobeedev <moha.saoud22@gmail.com>
2026-03-10 12:19:31 +0000

    exercise ex12

--- 02_variables_data_types/ex12/solution.c
+++ 02_variables_data_types/ex12/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/10 12:15:51 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/10 12:18:23 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/10 12:19:15 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main()
 {
-	long long int nbr = 9223372036854775807;
+	long long int nbr = 9223372036854775807LL;
 
 	printf("nbr = %lld\n",nbr);
 
