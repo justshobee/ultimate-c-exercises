4a35bee6ca103086803a6ad3efb3bf909349cd91
shobeedev <moha.saoud22@gmail.com>
2026-03-10 11:35:16 +0000

    exercise ex04

---
+++ 02_variables_data_types/ex04/solution.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/10 11:30:30 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/10 11:34:35 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Size of int : %d bytes.\n",sizeof(int));
+	printf("Size of float : %d bytes.\n",sizeof(float));
+	printf("Size of double : %d bytes.\n",sizeof(double));
+	printf("Size of char : %d bytes.\n",sizeof(char));
+	printf("Size of long : %d bytes.\n",sizeof(long));
+	printf("Size of long long : %d bytes.\n",sizeof(long long));
+	printf("Size of long double : %d bytes.\n",sizeof(long double));
+
+	return 0;
+}
