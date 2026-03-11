93cf62a3550cf5a69006207b57f488ab86ff8877
shobeedev <moha.saoud22@gmail.com>
2026-03-11 12:11:48 +0000

    exercise ex09

--- 03_loops/ex09/solution.c
+++ 03_loops/ex09/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/03/11 11:57:05 by shobeedev               shobee4ever      */
-/*   Updated: 2026/03/11 12:09:04 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/03/11 12:11:29 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int main()
 	char letter = (ch >= 'A' && ch <= 'Z') ? ch + 32 : ch;
 	switch(letter)
 	{
-		case 'a':printf("%c is a Vowel.\n",ch); break;
-		case 'i':printf("%c is a Vowel.\n",ch); break;
-		case 'o':printf("%c is a Vowel.\n",ch); break;
-		case 'e':printf("%c is a Vowel.\n",ch); break;
-		case 'u':printf("%c is a Vowel.\n",ch); break;
+		case 'a':
+		case 'i':
+		case 'o':
+		case 'e':
+		case 'u':
+			printf("%c is a Vowel.\n",ch); break;
 		default :printf("%c is a Consonant.\n",ch);
 	}
 
