ec02c1a77e62fd6caac0e78d6c5d4fe422c9163e
shobeedev <moha.saoud22@gmail.com>
2026-03-11 11:13:34 +0000

    exercise ex01

---
+++ 03_loops/ex01/solution.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/11 11:07:22 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/11 11:13:17 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main()
+{
+	int nbr;
+	printf("Enter aa number : ");
+	scanf("%d",&nbr);
+
+	if(nbr > 0)
+		printf("%d is a Positive number.\n", nbr);
+	else if(nbr < 0)
+		printf("%d is a Positive number.\n", nbr);
+	else
+		printf("%d is a Positive number.\n", nbr);
+
+	return 0;
+}
+
