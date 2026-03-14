0d5e57727291425f8cd08beaf882eac3a1b300f5
shobeedev <moha.saoud22@gmail.com>
2026-03-14 12:07:42 +0000

    exercise ex01

---
+++ 04_functions/ex01/solution.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/14 12:06:04 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/14 12:07:23 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+void greet()
+{
+	printf("Hello, C Functions!\n");
+}
+
+int main()
+{
+	greet();
+
+	return 0;
+}
