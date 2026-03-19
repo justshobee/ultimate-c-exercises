725ed13167304e5a4f8ac27ddb76877ffef3794e
shobeedev <moha.saoud22@gmail.com>
2026-03-19 22:27:17 +0000

    exercise ex09

---
+++ 05_arrays/ex09/solution.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                           ____    _____    */
+/*   solution.c                                             |___ \  |___ /    */
+/*                                                            __) |   |_ \    */
+/*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
+/*                                                          |_____| |____/    */
+/*   Created: 2026/03/19 21:25:48 by shobeedev               shobee4ever      */
+/*   Updated: 2026/03/19 22:27:03 by shobeedev            tfaaty fi l3oolaa   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main()
+{
+	int size = 7;
+	int arr[7] = {10, 50, 20, 80, 40, 90, 30};
+
+	printf("Array elements: ");
+	int i = 0;
+	while(i < size)
+	{
+		printf("%d ",arr[i]);
+		i++;
+	}
+	printf("\n");
+
+	int max1 = -2147483648;
+	int max2 = -2147483648;
+
+	int min1 = 2147483647;
+	int min2 = 2147483647;
+
+	i = 0;
+	while(i < size)
+	{
+		if(arr[i] > max1)
+		{
+			max2 = max1;
+			max1 = arr[i];
+		}else if(arr[i] > max2 && arr[i] != max1)
+			max2 = arr[i];
+
+
+		if(arr[i] < min1)
+		{
+			min2 = min1;
+			min1 = arr[i];
+		}else if(arr[i] < min2 && arr[i] != min1)
+			min2 = arr[i];
+
+		i++;
+
+	}
+
+
+	printf("Second Largest (Max): %d\n",max2);
+	printf("Second Smallest (Min): %d\n",min2);
+
+	return 0;
+}
