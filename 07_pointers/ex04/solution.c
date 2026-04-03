9258425f3e56a91c6d13ce12c59d76de952ab54b
shobeedev <moha.saoud22@gmail.com>
2026-04-03 10:25:33 +0100

    exercise ex04

--- 07_pointers/ex04/solution.c
+++ 07_pointers/ex04/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/04/03 07:46:13 by shobeedev               shobee4ever      */
-/*   Updated: 2026/04/03 10:23:39 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/04/03 10:25:01 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int main()
 {
-	printf("Size of int pointer (int *) : %d bytes\n",sizeof(int *));
-	printf("Size of float pointer (float *) : %d bytes\n",sizeof(float *));
-	printf("Size of char pointer (char *) : %d bytes\n\n",sizeof(char *));
-	printf("Size of int pointer (int) : %d bytes\n",sizeof(int));
-	printf("Size of float pointer (float) : %d bytes\n",sizeof(float));
-	printf("Size of char pointer (char) : %d bytes\n",sizeof(char));
+	printf("Size of int pointer (int *) : %zu bytes\n",sizeof(int *));
+	printf("Size of float pointer (float *) : %zu bytes\n",sizeof(float *));
+	printf("Size of char pointer (char *) : %zu bytes\n\n",sizeof(char *));
+	printf("Size of int pointer (int) : %zu bytes\n",sizeof(int));
+	printf("Size of float pointer (float) : %zu bytes\n",sizeof(float));
+	printf("Size of char pointer (char) : %zu bytes\n",sizeof(char));
     return 0;	
 }
