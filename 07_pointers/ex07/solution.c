f437d389f9c60ddaea08e29a1388f474baf3e9ac
shobeedev <moha.saoud22@gmail.com>
2026-04-03 12:06:17 +0100

    exercise ex07

--- 07_pointers/ex07/solution.c
+++ 07_pointers/ex07/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/04/03 11:57:23 by shobeedev               shobee4ever      */
-/*   Updated: 2026/04/03 12:05:06 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/04/03 12:06:07 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 int main()
 {
-	int data[5] = {1, 3, 5, 7, 9};
+    int data[5] = {1, 3, 5, 7, 9};
+    int *ptr = data; // Pointer initialized to array base address
 
-	int *ptr = &data[0];
-	printf("Array element at index 2 (Value should be 5):\n");
-	printf("1. Subscript notation : %d\n",data[2]);
-	printf("2. Pointer notation (arr + 2): %d\n",*(data + 2));
-	printf("3. Pointer notation (ptr + 2): %d\n",*(ptr + 2));
+    printf("Array element at index 2 (Value should be 5):\n");
+
+    // Method 1: Subscript notation (Standard Array access)
+    printf("1. Subscript notation: %d\n", data[2]);
+
+    // Method 2: Pointer notation with the base address (Array name as pointer)
+    printf("2. Pointer notation (arr + 2): %d\n", *(data + 2));
+
+    // Method 3: Pointer notation with the offset pointer
+    printf("3. Pointer notation (ptr + 2): %d\n", *(ptr + 2));
+
+    return 0;
 }
