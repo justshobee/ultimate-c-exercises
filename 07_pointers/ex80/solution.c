be92705e97c2683cb2c6a1410dd60569edec0cb4
shobeedev <moha.saoud22@gmail.com>
2026-04-08 18:44:54 +0100

    exercise ex80

--- 07_pointers/ex80/solution.c
+++ 07_pointers/ex80/solution.c
@@ -6,7 +6,7 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/04/08 18:29:39 by shobeedev               shobee4ever      */
-/*   Updated: 2026/04/08 18:39:34 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/04/08 18:44:41 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,29 @@ int count_words(char *s)
 	}
 	return count;
 }
+int count_words_m2(char *s)
+{
+	int i = 0;
+	int counter = 0;
+	bool isword = 0;
+	while(s[i] != '\0')
+	{
+		if(is_space(s[i]))
+			isword = 0;
+		else if(isword == 0)
+		{
+			isword = 1;
+			counter++;
+		}
+		i++;
+	}
+	return counter;
+}
 int main(int count,char *str[])
 {
 	if(count == 2)
 	{
-		int counter = count_words(str[1]);
+		int counter = count_words_m2(str[1]);
 		printf("The string is : \"%s\"\n",str[1]);
 		printf("Total number of words : %d\n",counter);
 	}
