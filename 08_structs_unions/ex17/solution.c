4ee82785fe6ae027bf0be08bffb2651e4371d559
shobeedev <moha.saoud22@gmail.com>
2026-05-06 18:37:29 +0100

    ex17

--- 08_structs_unions/ex17/solution.c
+++ 08_structs_unions/ex17/solution.c
@@ -6,12 +6,23 @@
 /*   By: shobeedev <https://shobee.space/>                   / __/   ___) |   */
 /*                                                          |_____| |____/    */
 /*   Created: 2026/05/06 18:25:14 by shobeedev               shobee4ever      */
-/*   Updated: 2026/05/06 18:35:50 by shobeedev            tfaaty fi l3oolaa   */
+/*   Updated: 2026/05/06 18:37:20 by shobeedev            tfaaty fi l3oolaa   */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <stdio.h>
+
+
+
+/*Bit fields allow us to specify the exact width of a structure member in bits.
+ * By setting each flag to : 1, we instruct the compiler to pack these three 
+ * flags into the smallest possible memory container, typically an integer 
+ * (4 bytes) or the smallest addressable unit (1 byte, if permitted).*/
+
+/*While the total occupied memory is small (usually 4 bytes for alignment),
+ * only 3 bits are used for actual data storage, drastically saving memory
+ * when dealing with millions of such structures.*/
 
 typedef struct	st_permissions
 {
