188f982dc3af84634302b4c3a31777955fa7ec62
shobeedev <moha.saoud22@gmail.com>
2026-08-19 11:23:43 +0100

    delete node

---
+++ 11_linked_lists/ex07/README.md
@@ -0,0 +1,22 @@
+7. Delete Head Variants
+
+Write a program in C to delete the first node of a Singly Linked List.
+
+Test Data :
+
+Input the number of nodes : 3
+Input data for node 1 : 2
+Input data for node 2 : 3
+Input data for node 3 : 4
+Expected Output :
+
+ Data entered in the list are :                                                                               
+ Data = 2                                                                                                     
+ Data = 3                                                                                                     
+ Data = 4                                                                                                     
+                                                                                                              
+ Data of node 1 which is being deleted is :  2                                                                
+                                                                                                              
+ Data, after deletion of first node :                                                                         
+ Data = 3                                                                                                     
+ Data = 4 
---
+++ 11_linked_lists/ex07/solution.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   solution.c                                // anyone can copy the code    */
+/*                                                                            */
+/*   By: shobeedev                             // but only understanding      */
+/*      <https://github.com/justshobee>        // makes it yours.             */
+/*                                                                            */
+/*   Created: 2026/08/19 10:47:24 by shobeedev // learn the why,              */
+/*   Updated: 2026/08/19 11:21:58 by shobeedev // not only the how.           */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+void	insert_node(node **head, int data)
+{
+	node	*tmp;
+	node	*ptr;
+
+	tmp = malloc(sizeof(node));
+	tmp->data = data;
+	tmp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return ;
+	}
+	ptr = *head;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next = tmp;
+}
+
+void	print_lst(node *head)
+{
+	node	*ptr;
+
+	ptr = head;
+	if (!ptr)
+	{
+		printf("the list is empty!\n");
+		return ;
+	}
+	while (ptr != NULL)
+	{
+		printf("%d | %p\n", ptr->data, ptr->next);
+		ptr = ptr->next;
+	}
+}
+
+node	*free_lst(node *head)
+{
+	node	*next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+	head = NULL;
+	return (head);
+}
+
+int		main(int size, char **str)
+{
+	if (size > 1)
+	{
+		node	*head;
+		int		i;
+
+		head = NULL;
+		i = 1;
+		while (i < size)
+		{
+			insert_node(&head, atoi(str[i]));
+			i++;
+		}
+		print_lst(head);
+		head = free_lst(head);
+	}
+}
