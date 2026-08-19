7679cfe936220774aa9e3d9a567db3c50a8779de
shobeedev <moha.saoud22@gmail.com>
2026-08-19 10:00:31 +0100

    insert node at beginning

--- 11_linked_lists/ex04/solution.c
+++ 11_linked_lists/ex04/solution.c
@@ -6,11 +6,9 @@
 /*      <https://github.com/justshobee>        // makes it yours.             */
 /*                                                                            */
 /*   Created: 2026/08/19 09:56:21 by shobeedev // learn the why,              */
-/*   Updated: 2026/08/19 09:56:27 by shobeedev // not only the how.           */
+/*   Updated: 2026/08/19 10:00:26 by shobeedev // not only the how.           */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
