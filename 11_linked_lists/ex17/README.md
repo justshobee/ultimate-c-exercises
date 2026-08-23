51982cb315ef0054544ea446c3bf04a2de6d6b2e
shobeedev <moha.saoud22@gmail.com>
2026-08-23 20:12:18 +0100

    sort a singly linked list

---
+++ 11_linked_lists/ex17/README.md
@@ -0,0 +1,11 @@
+17. Linked List Sorting Challenges
+
+Write a C program to sort a singly linked list.
+
+Test Data and Expected Output :
+
+Sort the said singly linked list:
+2 3 1 7 5 
+
+After sorting the said list:
+1 2 3 5 7 
---
+++ 11_linked_lists/ex17/list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   list.h                                    // anyone can copy the code    */
+/*                                                                            */
+/*   By: shobeedev                             // but only understanding      */
+/*      <https://github.com/justshobee>        // makes it yours.             */
+/*                                                                            */
+/*   Created: 2026/08/22 09:00:07 by shobeedev // learn the why,              */
+/*   Updated: 2026/08/22 09:00:12 by shobeedev // not only the how.           */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIST_H
+# define LIST_H
+
+struct st_node
+{
+	int		data;
+	struct st_node	*next;
+};
+
+typedef struct st_node node;
+
+#endif
