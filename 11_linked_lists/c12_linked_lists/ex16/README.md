9e85e671880ba7f690263dd98022679b3104cae6
shobeedev <moha.saoud22@gmail.com>
2026-08-28 16:53:38 +0100

    ft_sorted_list_insert

---
+++ 11_linked_lists/c12_linked_lists/ex16/README.md
@@ -0,0 +1,8 @@
+## 16. ft_sorted_list_insert
+ 
+* **Description:** Create the function `ft_sorted_list_insert`, which creates a new element and inserts it into an already-sorted list so that the list remains sorted in ascending order.
+* **Directory:** `ex16/`
+* **Files to submit:** `ft_sorted_list_insert.c`, `ft_list.h`
+* **Authorized:** `ft_create_elem`
+* **Function Prototype:** `void ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)());`
+* **Usage note:** `cmp` is used as `(*cmp)(list_ptr->data, list_other_ptr->data);`.
---
+++ 11_linked_lists/c12_linked_lists/ex16/solution.c
@@ -0,0 +1,236 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   solution.c                                // anyone can copy the code    */
+/*                                                                            */
+/*   By: shobeedev                             // but only understanding      */
+/*      <https://github.com/justshobee>        // makes it yours.             */
+/*                                                                            */
+/*   Created: 2026/08/28 15:52:58 by shobeedev // learn the why,              */
+/*   Updated: 2026/08/28 16:51:57 by shobeedev // not only the how.           */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include "list.h"
+
+int		count_list(t_list *head)
+{
+	int		count;
+
+	count = 0;
+	while (head)
+	{
+		count++;
+		head = head->next;
+	}
+	return (count);
+}
+
+int		ft_strcmp(void *s1, void *s2)
+{
+	while (*(char *)s1 == *(char *)s2 && *(char *)s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(char *)s1 - *(char *)s2);
+}
+
+int		ft_ncmp(void *a, void *b)
+{
+	return (*(int *)a - (*(int *)b));
+}
+
+void ft_sorted_list_insert(t_list **head, void *data,  int (*cmp)(void*, void*))
+{
+	t_list	*tmp;
+	t_list	*ptr;
+	t_list	*prev;
+
+	tmp = malloc(sizeof(t_list));
+	tmp->data = data;
+	tmp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = tmp;
+		return ;
+	}
+	ptr = *head;
+	prev = NULL;
+	while (ptr->next)
+	{
+		if (cmp(ptr->data, data) > 0)
+		{
+			if (prev != NULL)
+				prev->next = tmp;
+			else
+				*head = tmp;
+			tmp->next = ptr;
+			return ;
+		}
+		prev = ptr;
+		ptr = ptr->next;
+	}
+	ptr->next = tmp;
+}
+
+void	bubble_sort(void *arr[], int size, int (*cmp) (void*, void*))
+{
+	void	*tmp;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (cmp(arr[j], arr[j + 1]) > 0)
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void ft_list_sort(t_list **head, int (*cmp)(void*, void*))
+{
+	t_list	*ptr;
+	int		size;
+	int		i;
+	void	*arr[count_list(*head)];
+
+	if (*head == NULL)
+		return ;
+	ptr = *head;
+	size = count_list(*head);
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = ptr->data;
+		ptr = ptr->next;
+		i++;
+	}
+	bubble_sort(arr, size, cmp);
+	i = 0;
+	ptr = *head;
+	while (ptr)
+	{
+		ptr->data = arr[i];
+		ptr = ptr->next;
+		i++;
+	}
+}
+
+void	push_front(t_list **head, void *data)
+{
+	t_list	*tmp;
+
+	tmp = malloc(sizeof(t_list));
+	tmp->data = data;
+	tmp->next = *head;
+	*head = tmp;
+}
+
+void	print_lst(t_list *head, char type)
+{
+	if (!head)
+	{
+		printf("empty list!!\n");
+		return ;
+	}
+	while (head)
+	{
+		if (type == 'i')
+			printf("%d ", *(int *)head->data);
+		else if (type == 'c')
+			printf("%c ", *(char *)head->data);
+		else
+			printf("%s\n", (char *)head->data);
+		head = head->next;
+	}
+	printf("\n");
+}
+
+void	free_lst(t_list *head, int free_data)
+{
+	t_list	*next;
+
+	while (head)
+	{
+		next = head->next;
+		if (free_data)
+			free(head->data);
+		free(head);
+		head = next;
+	}
+}
+
+void	test_int()
+{
+	srand((unsigned)time(NULL));
+	t_list	*head;
+	int		*data;
+	int		size;
+	int		i;
+
+	head = NULL;
+	data = NULL;
+	size = rand() % (10 + 1);
+	i = 0;
+	while (i < size)
+	{
+		data = malloc(sizeof(int));
+		//range (end - start + 1) + start
+		*data = rand() % (CHAR_MAX + 1);
+		//*data = rand() % INT_MAX;
+		push_front(&head, data);
+		i++;
+	}
+	print_lst(head, 'i');
+	ft_list_sort(&head, ft_ncmp);
+	data = malloc(sizeof(int));
+	*data = rand() % (CHAR_MAX + 1);
+	printf("The element : %d\n", *data);
+	ft_sorted_list_insert(&head, data, ft_ncmp);
+	print_lst(head, 'i');
+	free_lst(head, 1);
+}
+
+void	test_str(int size, char **str)
+{
+	t_list	*head;
+	int		i;
+
+	i = 1;
+	head = NULL;
+	while (i < size)
+	{
+		push_front(&head, str[i]);
+		i++;
+	}
+	print_lst(head, 's');
+	ft_list_sort(&head, ft_strcmp);
+	printf("The element : %s\n\n", str[0]);
+	ft_sorted_list_insert(&head, str[0], ft_strcmp);
+	print_lst(head, 's');
+	free_lst(head, 0);
+}
+
+int		main(int size, char **str)
+{
+	if (size > 1)
+		test_str(size - 1, str + 1);
+	else
+		test_int();
+	return (0);
+}
+
